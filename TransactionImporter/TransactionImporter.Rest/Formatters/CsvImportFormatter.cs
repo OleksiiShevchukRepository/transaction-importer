f57e5910ba66dd8f1bcffc241b58e84988f04103
oleksii.shevchuk <oleksii.shevchuk.n@gmail.com>
2020-04-17 10:38:18 +0300

    add csv/xml formatters

---
+++ TransactionImporter/TransactionImporter.Rest/Formatters/CsvImportFormatter.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TransactionImporter.Rest.Formatters
+{
+    public class CsvImportFormatter
+    {
+        private readonly CsvFormatterOptions _options;
+        private readonly bool useJsonAttributes = true;
+        public CsvImportFormatter() => _options = new CsvFormatterOptions();
+
+        public async Task<List<T>> ReadStreamCsvAsync<T>(IFormFile formFile) where T : class
+        {
+            var constructedListType = typeof(List<>).MakeGenericType(typeof(T));
+            var list = (List<T>)Activator.CreateInstance(constructedListType);
+
+            using (var stream = formFile.OpenReadStream())
+            {
+                bool skipFirstLine = _options.UseSingleLineHeaderInCsv;
+                using (var reader = new StreamReader(stream, _options.Encoding))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = await reader.ReadLineAsync();
+                        var values = line.Split(_options.CsvDelimiter.ToCharArray());
+
+                        if (skipFirstLine)
+                            skipFirstLine = false;
+                        else
+                        {
+                            var itemTypeInGeneric = list.GetType().GetTypeInfo().GenericTypeArguments[0];
+                            var item = Activator.CreateInstance(itemTypeInGeneric);
+                            var properties = useJsonAttributes
+                                ? item.GetType().GetProperties().Where(pi => !pi.GetCustomAttributes<JsonIgnoreAttribute>().Any()).ToArray()
+                                : item.GetType().GetProperties();
+
+                            for (int i = 0; i < values.Length; i++)
+                            {
+                                object propertyValue;
+                                Type propertyType = properties[i].PropertyType;
+                                Type underlyingType = Nullable.GetUnderlyingType(propertyType) ?? propertyType;
+
+                                if (underlyingType.IsEnum)
+                                    propertyValue = string.IsNullOrEmpty(values[i]) || !Enum.IsDefined(underlyingType, values[i])
+                                        ? default : Enum.Parse(underlyingType, values[i]);
+                                else
+                                    propertyValue = string.IsNullOrEmpty(values[i]) ? null : Convert.ChangeType(values[i], underlyingType);
+
+                                properties[i].SetValue(item, propertyValue, null);
+                            }
+
+                            list.Add((T)item);
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ TransactionImporter/TransactionImporter.Rest/Formatters/XmlImportFormatter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace TransactionImporter.Rest.Formatters
+{
+    public class XmlImportFormatter
+    {
+        public async Task<List<T>> ReadStreamXmlAsync<T>(IFormFile formFile) where T : class
+        {
+            var model = new List<T>();
+            using (var readStream = formFile.OpenReadStream())
+            {
+                using (var streamReader = new StreamReader(readStream))
+                {
+                    var xmlDoc = await XDocument.LoadAsync(streamReader, LoadOptions.None, CancellationToken.None);
+                    xmlDoc.Descendants().Where(e => string.IsNullOrEmpty(e.Value)).Remove();
+
+                    var serializer = new XmlSerializer(typeof(T));
+                    foreach (var node in xmlDoc.Root.Nodes())
+                    {
+                        var element = serializer.Deserialize(node.CreateReader());
+                        model.Add((T)element);
+                    }
+                }
+            }
+
+            return model;
+        }
+    }
+}
