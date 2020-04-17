620826f2623b70408d0ccfcd650eea9a72fdc110
oleksii.shevchuk <oleksii.shevchuk.n@gmail.com>
2020-04-17 10:18:16 +0300

    add TransactionDbContext, entities and configurations

---
+++ TransactionImporter/TransactionImporter.DataAccess/Entities/TransactionStatusEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace TransactionImporter.DataAccess.Entities
+{
+    public enum TransactionStatusEnum
+    {
+        A = 1, //Approved
+        R,  //Rejected
+        D //Done
+    }
+}
---
+++ TransactionImporter/TransactionImporter.DataAccess/TransactionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DummyMvc.DataAccess.EntitiesConfigurations;
+using Microsoft.EntityFrameworkCore;
+using TransactionImporter.DataAccess.Entities;
+
+namespace DummyMvc.DataAccess
+{
+    public class TransactionDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public TransactionDbContext(DbContextOptions options) : base(options)
+           => Database.EnsureCreated();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfiguration(new TransactionEntityConfiguration());
+    }
+}
